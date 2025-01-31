a486d256e1a9c17375bf2b5818b8f77955b3792f
jjalixto <jjalixtoc@gmail.com>
2025-01-31 18:17:14 -0500

    project creation

---
+++ src/test/java/com/joel/reto_tecnico_entelgy/RetoTecnicoEntelgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.reto_tecnico_entelgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoTecnicoEntelgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
